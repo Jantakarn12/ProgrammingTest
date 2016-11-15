c77b84b8c38529f2b7f0fa17fcbf7bdd51e8c2ee
Khimmy <annani12@hotmail.com>
2016-11-15 15:12:30 +0700

    580610625 new2

--- src/User.java
+++ src/User.java
@@ -4,6 +4,12 @@ public class User implements IUser {
     protected String password;
     protected int type;
 
+
+    public  String passwordform = "^[a-zA-Z][a-zA-Z0-9]{12,}";
+
+
+
+
     public User(int type, String name, String password) {
         this.type = type;
         setName(name);
@@ -18,29 +24,33 @@ public class User implements IUser {
 
     @Override
     public String setName(String name) {
-        if (name.matches("^[a-zA-Z][a-zA-Z0-9]{7,}$")) {
-            this.name = name;
-
-            return this.name;
-
-        } else if (name.matches("")) {
+        if(name.equals(""))
+        {
             System.out.print("Wrong username");
             return null;
-
-        } else {
+        }
+        else if (name.matches("^[a-zA-Z][a-zA-Z0-9]{7,}$")) {
+            this.name = name;
+            return this.name;
+        }
+        else
+        {
             return this.name;
         }
     }
 
     @Override
     public String setPassword(String password) {
-
-        if (password.matches("^[a-zA-Z][a-zA-Z0-9]{12,}")) {
-            this.password = password;
-
+        if(password.equals(""))
+        {
+            System.out.print("Wrong password");
             return this.password;
-
+        }
+        else if(password.matches("^(?=.*[a-zA-Z])(?=.*[a-zA-Z0-9]){12,}$")){
+            this.password = password;
+            return this.password;
         } else {
+            System.out.print("Wrong password");
             return this.password;
         }
     }
